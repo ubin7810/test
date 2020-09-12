8a1120df63a1088410897f18cbf4778b59d2fed1
ubin7810 <71161261+ubin7810@users.noreply.github.com>
2020-09-12 11:20:37 +0900

    Create index.py

---
+++ index.py
@@ -0,0 +1,39 @@
+print('Hello World')
+a=10
+print(a)
+name = 'mike'
+print(name)
+city = 'seoul'
+a = 10.5
+print(a)
+live_in_seoul = True
+print(live_in_seoul)
+live_in_tokyo = False
+print(live_in_tokyo)
+names = ['mike', 'jane', 'tom']
+print(names)
+print(names[0])
+odd_number = [1,3,5,7,9]
+print(odd_number)
+user = {}
+user['age'] = 25
+user['address'] = 'seoul'
+print(user)
+print(user['age'])
+print(user.get('age')) 
+user['age'] = 90
+print(user.get('age'))
+live_in_soeul = True
+if live_in_seoul:
+    print('서울에 살아요')
+live_in_seoul = False
+if live_in_seoul:
+    print('서울에 살아요')
+name = 'jane'
+if name == 'jane':
+    print('이름이 제인이에요')
+else:
+    print('이름이 제인이 아니에요')
+for num in odd_number:
+    print(odd_number)
+    print(num)
